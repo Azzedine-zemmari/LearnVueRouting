0da120661b4983b7ec1292fd80eb7a095e931e7f
Azzedine zemmari <98091150+Azzedine-zemmari@users.noreply.github.com>
2024-02-06 21:27:48 +0100

    end the first lesson i add all the destination in the Home.vue using routerLink

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>All Destination</h1>
+    <div class="destinations">
+      <RouterLink 
+      v-for="destination in destinations"
+      :key="destination.id"
+      :to="destination.slug"
+      >
+      {{ destination.name }}
+      <img :src="`/images/${destination.image}`" :alt="destination.name">
+    </RouterLink>
+    </div>
   </div>
 </template>
 <script setup>
-import sourceData from '../data.json'
-const destination = sourceData.destinations
+import sourceData from '@/data.json'
+const destinations = sourceData.destinations
 </script>
