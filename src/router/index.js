915f877b24d4e44114a9ea484036ed9e73c25971
Azzedine zemmari <98091150+Azzedine-zemmari@users.noreply.github.com>
2024-02-06 21:42:14 +0100

    the lesson 2 Difference Between createWebHashHistory and createWebHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Brazil from '@/views/Brazil.vue'
@@ -6,40 +6,42 @@ import jamaica from '@/views/Jamaica.vue'
 import Hawai from '@/views/Hawai.vue'
 import Panama from '@/views/Panama.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+  {
+    path: '/brazil',
+    name: 'brazil',
+    component: Brazil
+  },
+  {
+    path: '/hawai',
+    name: 'hawai',
+    component: Hawai
+  },
+  {
+    path: '/jamaica',
+    name: 'jamaica',
+    component: jamaica
+  },
+  {
+    path: '/panama',
+    name: 'panama',
+    component: Panama
+  },
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    },
-    {
-      path: '/brazil',
-      name: 'brazil',
-      component: Brazil
-    },
-    {
-      path: '/hawai',
-      name: 'hawai',
-      component: Hawai
-    },
-    {
-      path: '/jamaica',
-      name: 'jamaica',
-      component: jamaica
-    },
-    {
-      path: '/panama',
-      name: 'panama',
-      component: Panama
-    },
-  ]
+  history: createWebHashHistory(),
+  routes
 })
 
 export default router
